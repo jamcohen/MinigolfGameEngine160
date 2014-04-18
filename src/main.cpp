5cebc0b9556fca9c76f9c682b44d1ffa758d545a
kirbyau <orlando.salvatore@me.com>
2014-04-18 12:29:46 -0700

    Removed stupid graphics class

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <OpenGL/gl3.h>
+#include "Drawing.h"
 #include <ctime>
-
-class Graphics
-{
-public:
-    
-    void update()
-    {
-        glClearColor(rand() % 255 / 255.0f, rand() % 255 / 255.0f, rand() % 255 / 255.0f, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    }
-};
 
 /*
  * Starts everything
@@ -53,7 +43,7 @@ int main(int argc, char * arg[])
     bool running = true;
     SDL_Event mainEvent;
     auto gl = SDL_GL_CreateContext(window);
-    Graphics *graphics = new Graphics();
+    Drawing *graphics = new Drawing();
 
     //TO-DO: We are going to have to work on setting the update based on the drawing FPS
     while(running)
